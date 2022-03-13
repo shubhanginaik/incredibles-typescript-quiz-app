101633d58dd958a2df4581ba28d06c98a13151e1
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-03-13 13:27:06 +0200

    added basic QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import QuestionCard from './components/QuestionCard';
 
 function App() {
   return (
     <div className="App">
   <h1>QUIZ APP</h1>
+  <QuestionCard />
     </div>
   );
 }

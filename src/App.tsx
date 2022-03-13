1309f8ea4c78725c72cff80bc2cfd73aef7ac05d
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-03-13 15:54:17 +0200

    startTrivia is working

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,12 @@ const TOTAL_QUESTIONS=10;
 function App() {
   const [loading ,setLoading] = useState(false);
   const [gameOver, setGameOver] = useState(true);
+  const [questions,setQuestions]= useState();
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS,Difficulty.EASY));
+  //console.log(fetchQuizQuestions(TOTAL_QUESTIONS,Difficulty.EASY));
 
-  const startTrivia = async ()=>{
+  const startTrivia = async (e:any)=>{
+    e.preventDefault()
     setLoading(true);
     setGameOver(false);
 
@@ -23,13 +25,15 @@ function App() {
       TOTAL_QUESTIONS,
       Difficulty.EASY,
     );
+    console.log(newQuestions)
+    setQuestions(newQuestions)
       setLoading(false);
   }
 
   return (
     <div className="App">
   <h1>QUIZ APP</h1>
-  <button>Start</button>
+  <button className="start" onClick={startTrivia}>Start</button>
   <p className="score">Score:</p>
   <QuestionCard />
   <button>Next question</button>

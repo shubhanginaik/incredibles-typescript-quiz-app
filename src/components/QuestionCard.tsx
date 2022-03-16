73ad779bfa9e8b74d426bb70140eabf0e049b0bd
BC-macair011 <mahialemu@gmail.com>
2022-03-16 12:44:57 +0200

    QuestionCard functionality resolved

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,35 +1,46 @@
-import React from "react";
-type Props= {
-    question: string;
-    answers:string[];
-    callback:any;
-    userAnswer:any;
-    questionNo:number;
-    totalquestions:number;
+import React from 'react';
+// Types
+import { AnswerObj } from '../App';
 
-}
+// Styles
+import { ButtonWrapper } from './QuestionCard.styles';
 
-const QuestionCard: React.FC<Props>= ({question, answers,callback,userAnswer,questionNo,totalquestions}) =>
-(
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObj | undefined;
+  questionNo: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNo,
+  totalQuestions,
+}) => (
+  <div>
+    <p className='number'>
+      Question: {questionNo} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
-        <p className="number">
-            Question:{questionNo}/{totalquestions}
-        </p>
-
-        <p dangerouslySetInnerHTML={{__html:question}} />
-        <div>
-            {answers.map(answer=>(
-                <div>
-                    <button disabled={userAnswer} onClick={callback}>
-                        <span dangerouslySetInnerHTML={{__html:answer}}></span>
-                    </button>
-                </div>
-
-            ))}
-        </div>
+      {answers.map((answer) => (
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
     </div>
-)
-     
-
+  </div>
+);
 
 export default QuestionCard;
